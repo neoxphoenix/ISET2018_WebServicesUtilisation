2b4848e3947e75973c6de3065abde3ab200059bd
Jonathan WUYTENS <Jonathan.WUYTENS@student.hel.be>
2019-01-30 11:54:44 +0100

    Add project files.

---
+++ ISET2018_WebServicesUtilisation/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace ISET2018_WebServicesUtilisation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHello = new System.Windows.Forms.Button();
+            this.lblID = new System.Windows.Forms.Label();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.btnPersonneByID = new System.Windows.Forms.Button();
+            this.tbCheckPersonne = new System.Windows.Forms.TextBox();
+            this.tbCheckMDP = new System.Windows.Forms.TextBox();
+            this.lblPersonne = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnVerifier = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbCodeISBN = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnVerifierISBN = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnHello
+            // 
+            this.btnHello.Location = new System.Drawing.Point(0, 0);
+            this.btnHello.Name = "btnHello";
+            this.btnHello.Size = new System.Drawing.Size(251, 42);
+            this.btnHello.TabIndex = 0;
+            this.btnHello.Text = "Hello";
+            this.btnHello.UseVisualStyleBackColor = true;
+            this.btnHello.Click += new System.EventHandler(this.btnHello_Click);
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(9, 45);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(101, 13);
+            this.lblID.TabIndex = 1;
+            this.lblID.Text = "Identifiant Personne";
+            // 
+            // tbID
+            // 
+            this.tbID.Location = new System.Drawing.Point(0, 61);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(251, 20);
+            this.tbID.TabIndex = 2;
+            this.tbID.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnPersonneByID
+            // 
+            this.btnPersonneByID.Location = new System.Drawing.Point(0, 87);
+            this.btnPersonneByID.Name = "btnPersonneByID";
+            this.btnPersonneByID.Size = new System.Drawing.Size(251, 42);
+            this.btnPersonneByID.TabIndex = 3;
+            this.btnPersonneByID.Text = "Chercher (par ID)";
+            this.btnPersonneByID.UseVisualStyleBackColor = true;
+            this.btnPersonneByID.Click += new System.EventHandler(this.btnPersonneByID_Click);
+            // 
+            // tbCheckPersonne
+            // 
+            this.tbCheckPersonne.Location = new System.Drawing.Point(0, 175);
+            this.tbCheckPersonne.Name = "tbCheckPersonne";
+            this.tbCheckPersonne.Size = new System.Drawing.Size(251, 20);
+            this.tbCheckPersonne.TabIndex = 4;
+            this.tbCheckPersonne.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbCheckMDP
+            // 
+            this.tbCheckMDP.Location = new System.Drawing.Point(0, 222);
+            this.tbCheckMDP.Name = "tbCheckMDP";
+            this.tbCheckMDP.Size = new System.Drawing.Size(251, 20);
+            this.tbCheckMDP.TabIndex = 5;
+            this.tbCheckMDP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblPersonne
+            // 
+            this.lblPersonne.AutoSize = true;
+            this.lblPersonne.Location = new System.Drawing.Point(12, 159);
+            this.lblPersonne.Name = "lblPersonne";
+            this.lblPersonne.Size = new System.Drawing.Size(102, 13);
+            this.lblPersonne.TabIndex = 6;
+            this.lblPersonne.Text = "Nom de la personne";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(12, 203);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(77, 13);
+            this.lblPassword.TabIndex = 7;
+            this.lblPassword.Text = "Mot de passe :";
+            // 
+            // btnVerifier
+            // 
+            this.btnVerifier.Location = new System.Drawing.Point(0, 248);
+            this.btnVerifier.Name = "btnVerifier";
+            this.btnVerifier.Size = new System.Drawing.Size(251, 42);
+            this.btnVerifier.TabIndex = 8;
+            this.btnVerifier.Text = "Verifier";
+            this.btnVerifier.UseVisualStyleBackColor = true;
+            this.btnVerifier.Click += new System.EventHandler(this.btnVerifier_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "________________________________________";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 293);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(247, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "________________________________________";
+            // 
+            // tbCodeISBN
+            // 
+            this.tbCodeISBN.Location = new System.Drawing.Point(0, 341);
+            this.tbCodeISBN.Name = "tbCodeISBN";
+            this.tbCodeISBN.Size = new System.Drawing.Size(251, 20);
+            this.tbCodeISBN.TabIndex = 11;
+            this.tbCodeISBN.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 325);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Verifier code  ISBN 13";
+            // 
+            // btnVerifierISBN
+            // 
+            this.btnVerifierISBN.Location = new System.Drawing.Point(0, 367);
+            this.btnVerifierISBN.Name = "btnVerifierISBN";
+            this.btnVerifierISBN.Size = new System.Drawing.Size(251, 42);
+            this.btnVerifierISBN.TabIndex = 13;
+            this.btnVerifierISBN.Text = "Verifier";
+            this.btnVerifierISBN.UseVisualStyleBackColor = true;
+            this.btnVerifierISBN.Click += new System.EventHandler(this.btnVerifierISBN_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(0, 415);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(251, 20);
+            this.textBox4.TabIndex = 14;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(251, 438);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.btnVerifierISBN);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbCodeISBN);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnVerifier);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblPersonne);
+            this.Controls.Add(this.tbCheckMDP);
+            this.Controls.Add(this.tbCheckPersonne);
+            this.Controls.Add(this.btnPersonneByID);
+            this.Controls.Add(this.tbID);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.btnHello);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHello;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.Button btnPersonneByID;
+        private System.Windows.Forms.TextBox tbCheckPersonne;
+        private System.Windows.Forms.TextBox tbCheckMDP;
+        private System.Windows.Forms.Label lblPersonne;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnVerifier;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbCodeISBN;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnVerifierISBN;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ ISET2018_WebServicesUtilisation/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ISET2018_WebServicesUtilisation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHello_Click(object sender, EventArgs e)
+        {
+            UtiliserServiceHEL.ServiceHELClient s = new UtiliserServiceHEL.ServiceHELClient();
+            MessageBox.Show(s.HelloWorld());
+        }
+
+        private void btnPersonneByID_Click(object sender, EventArgs e)
+        {
+            int nID;
+            if (int.TryParse(tbID.Text, out nID)) //vérifie que la conversion se passe bien, et donc si on a bien entré un int ID
+            {
+                UtiliserServiceHEL.ServiceHELClient s = new UtiliserServiceHEL.ServiceHELClient();
+                UtiliserServiceHEL.WS_Personne p = s.GetPersonneByID(nID);
+                if (p.ID == 0)
+                    MessageBox.Show("Personne non enregistrée");
+                else
+                    MessageBox.Show(p.Prenom + " " + p.Nom + " (" + p.ID.ToString() + ")");
+            }
+            else MessageBox.Show("Entrer un entier");
+        }
+
+        private void btnVerifier_Click(object sender, EventArgs e)
+        {
+            UtiliserServiceHEL.ServiceHELClient s = new UtiliserServiceHEL.ServiceHELClient();
+            UtiliserServiceHEL.WS_Personne p = s.CheckPersonneMDP(tbCheckPersonne.Text.ToString(), tbCheckMDP.Text.ToString());
+
+            MessageBox.Show(p.Prenom);
+        }
+
+        private void btnVerifierISBN_Click(object sender, EventArgs e)
+        {
+            //  978-0-306-40615-?
+            try
+            {
+                string codeISBNTraite = String.Join("", tbCodeISBN.Text.Split('-'));
+
+                if (codeISBNTraite.Length == 13)
+                {
+                    UtiliserServiceHEL.ServiceHELClient s = new UtiliserServiceHEL.ServiceHELClient();
+                    UtiliserServiceHEL.WS_CheckISBN p = s.CheckISBNNumber(codeISBNTraite);
+                    /*
+                    int[] ISBN = new int[codeISBNTraite.Length];
+                    Console.WriteLine("T:" + codeISBNTraite.Length);
+
+                    for (int i = 0; i < codeISBNTraite.Length; i++)
+                    {
+                        ISBN[i] = Int32.Parse(codeISBNTraite.Substring(i, 1));
+                        Console.WriteLine(ISBN[i]);
+                    }
+
+                    calculISBN = 10 - ((ISBN[0] * 1 + ISBN[1] * 3 + ISBN[2] * 1 + ISBN[3] * 3 + ISBN[4] * 1 + ISBN[5] * 3 + ISBN[6] * 1 + ISBN[7] * 3 + ISBN[8] * 1 + ISBN[9] * 3 + ISBN[10] * 1 + ISBN[11] * 3) % 10);
+                    Console.WriteLine("OK : " + calculISBN);
+
+                    if (calculISBN == ISBN[12])
+                        Console.WriteLine("YES");
+                    else
+                        Console.WriteLine("NO");
+                    */
+
+                    MessageBox.Show("Validité du code : " + p.codeValide.ToString() + " (Check Digit : " + p.checkDigit.ToString() + ")");
+
+                }
+                else
+                    MessageBox.Show("Code ISBN invalide!");
+            }
+            catch
+            {
+                MessageBox.Show("Code ISBN invalide!");
+            }
+            
+            
+        }
+    }
+}
